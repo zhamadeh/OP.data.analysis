ad62fc66c4fcf8ffaebf1ceb9d2fe58db74f50b7
zhamadeh <zhamadeh@github.com>
2021-08-04 11:04:26 -0700

    test ssh

--- Thesis/09-plottingSummary.R
+++ Thesis/09-plottingSummary.R
@@ -13,6 +13,9 @@ breakpointSummary <- read.table("Input/06.sisterChromatidExchanges.txt",header=T
 
 
 
+########### PLOT #1: SCEs/CHR/LIB vs LENGTH ###############
+###########################################################
+
 breakpointSummary$gene <- as.factor(breakpointSummary$gene)
 breakpointSummary$library <- as.factor(breakpointSummary$library)
 
@@ -70,48 +73,65 @@ ggplot(tidy) + geom_point(aes(Length,sce_per_chr,group=gene,color=gene), na.rm=T
   theme(legend.position = c(0.25, 0.8),legend.title = element_blank())+
   scale_colour_grey()
 
-data_summary <- function(data, varname, groupnames){
-  require(plyr)
-  summary_func <- function(x, col){
-    c(mean = mean(x[[col]], na.rm=TRUE),
-      sd = sd(x[[col]], na.rm=TRUE))
-  }
-  data_sum<-ddply(data, groupnames, .fun=summary_func,
-                  varname)
-  data_sum <- rename(data_sum, c("mean" = varname))
-  return(data_sum)
-}
 tidy$Length<-as.numeric(tidy$Length)
-tidy$Chromosome<-as.factor(tidy$Chromosome)
 tidy$gene<-as.factor(tidy$gene)
-str(tidy)
 tidy <- select(tidy,-Chromosome)
-data_summary(data = tidy,varname = "sce_per_chr",
-             groupnames = c("gene","Length"))
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+  library(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
 
-tidy%>% group_by(c("gene","Length"))%>% summarize(sd(sce_per_chr))
-df2=ddply(tidy,~c(Length),summarise,sd=sd(sce_per_chr),mean=mean(sce_per_chr))
-df2$`c(Length)`<-as.numeric(df2$`c(Length)`)
+tgc=summarySE(tidy, measurevar="sce_per_chr", groupvars=c("Length"))
+tgc$Length=as.integer(tgc$Length)
 
-ggplot(df2, aes(x='c(Length)', y=mean)) + 
-  geom_line() +
-  geom_point()+
-  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2,
-                position=position_dodge(0.05))
-
-ggplot(tidy) + geom_errorbar(aes(x = Length,ymin=min(sce_per_chr),ymax=max(sce_per_chr),group=gene,color=gene), na.rm=TRUE)+
-  geom_smooth(aes(Length,sce_per_chr,group=gene,color=gene),se=F,method="lm", na.rm=TRUE,size=2)+
+ggplot(tgc, aes(x=Length, y=sce_per_chr)) + 
+  geom_errorbar(aes(ymin=sce_per_chr-se, ymax=sce_per_chr+se,width=5),size=1,color="grey") +
+  geom_smooth(method="lm",se=F,size=3,color="black") +
+  #geom_crossbar(aes(ymin=sce_per_chr-se, ymax=sce_per_chr+se))+
   theme_classic(base_size = 25) +
-  ylab("SCEs/CHR/LIB")+
-  xlab("CHR LENGTH")+
-  theme(legend.position = c(0.25, 0.8),legend.title = element_blank())+
-  scale_colour_grey()
+  ylab("SCE Frequency")+
+  xlab("Chromosome Length")+
+  geom_point(size=3,color="blue")
 
 ggsave("Output/Plots/scePerChrPerGeneVsLength.png")
 
 
 
 
+
+
+################## Plot #2 ##############################
 
 suppressMessages(test<-as.data.frame(breakpointSummary %>%
                                        group_by(Library) %>%
@@ -198,3 +218,4 @@ suppressMessages(suppressWarnings(ggplot(breakpointSummary) + stat_ecdf(aes(widt
                                     geom_vline(xintercept=median(breakpointSummary$width), linetype="dashed", color = "red") +
                                     geom_text(aes(x=5000, label=paste0("Median\n",median(width)," bp"), y=0.8))  +
                                     ggsave("Output/Plots/breakpointResolution.png")))
+                                    
