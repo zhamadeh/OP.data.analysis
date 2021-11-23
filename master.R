80122179a7c1563155c18b449e8afdd3e0fe4be6
zhamadeh <zeid.hamadeh@hotmail.com>
2021-11-22 16:51:25 -0800

    new 2021 data

--- master.R
+++ master.R
@@ -21,9 +21,15 @@ runBreakpointR("../../Data/BAM/All_good_libraries/ALL_GOOD_BAM_FILES/")
 
 # Part 3
 breakpoints <- collectBreaksAllFiles()
-write.table(breakpoints,"Input/03.library.breakpoints.txt",col.names = T,row.names = F,quote = F,sep = "\t")
+breakpoints = breakpoints %>% separate(Library,c("Library","b"),"[.]") %>% select(-c(b,library))
+write.table(breakpoints,"INPUT/2021/03.library.breakpoints.txt",col.names = T,row.names = F,quote = F,sep = "\t")
 
-# Part 4
+s# Part 4
+bammetrics <- read.table("INPUT/2021/01.library.metrics_2021.txt",header=T)
+metrics <- read.table("INPUT/2021/00.metrics.summary_2021.txt",header=T)
+bammetrics = bammetrics %>% separate(file,c("Library","b"),"[.]") %>% select(-c(b))
+metrics_bam_summ = merge(metrics,bammetrics,by="Library")
+write.table(metrics_bam_summ,"INPUT/2021/02.metrics_bam_summ.txt",row.names = F,col.names = T,sep = "\t",quote = F)
 qualityMetrics()
 
 # Part 5
